f389220d7d9571d3ec9625add062beb46ad5159d
sanarWH <vkhv0702@gmail.com>
2023-11-22 03:39:59 +0300

    KS-10-1 solution

---
+++ src/main/kotlin/lesson10_task01/lesson10_task01.kt
@@ -0,0 +1,27 @@
+package lesson10_task01
+
+fun main() {
+    val userValue = getRandomNumber()
+    print("Ход человека. \nЗначения на кубиках: ")
+    Thread.sleep(1000)
+    println(userValue)
+
+    Thread.sleep(1000)
+
+    val pcValue = getRandomNumber()
+    print("Ход компьютера. \nЗначение на кубиках: ")
+    Thread.sleep(1000)
+    println(pcValue)
+
+    Thread.sleep(1000)
+
+    if (userValue > pcValue) println("Победил человек")
+    else if(userValue == pcValue) println("Ничья")
+    else println("Победил компьютер")
+}
+
+fun getRandomNumber(): Int{
+    val randomNumber = (1..6).random()
+    return randomNumber
+}
+
