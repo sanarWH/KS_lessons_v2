9cef68baf49aebc26dea08564e42ff3d7372353d
sanarWH <vkhv0702@gmail.com>
2023-11-27 05:15:47 +0300

    KS-13-2 solution

---
+++ src/main/kotlin/lesson13_task02/DataBaseV2.kt
@@ -0,0 +1,16 @@
+package lesson13_task02
+
+class DataBaseV2(var nameOfUser: String, var number: Long, var nameOfCompany: String? = "") {
+
+
+    fun getData(){
+        if (nameOfCompany == null || nameOfCompany == "") nameOfCompany = "не указано"
+
+        println("""
+            Имя: $nameOfUser
+            Телефон: $number
+            Компания: $nameOfCompany
+        """.trimIndent())
+    }
+
+}
---
+++ src/main/kotlin/lesson13_task02/lesson13_task02.kt
@@ -0,0 +1,6 @@
+package lesson13_task02
+
+fun main() {
+    val user1 = DataBaseV2("Mike", 89507639413)
+    user1.getData()
+}
