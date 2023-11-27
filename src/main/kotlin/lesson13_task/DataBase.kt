0dfa614f0c6e2a795876fb543be6c71549cee9b9
sanarWH <vkhv0702@gmail.com>
2023-11-27 04:51:09 +0300

    KS-13-1 solution

---
+++ src/main/kotlin/lesson13_task/DataBase.kt
@@ -0,0 +1,4 @@
+package lesson13_task
+
+class DataBase(var nameOfUser: String, var number: Long, var nameOfCompany: String?) {
+}
---
+++ src/main/kotlin/lesson13_task/lesson13_task01.kt
@@ -0,0 +1,8 @@
+package lesson13_task
+
+fun main() {
+    val user1 = DataBase("Mike", 89658534288, "")
+    println(user1.nameOfUser)
+    println(user1.number)
+    println(user1.nameOfCompany)
+}
