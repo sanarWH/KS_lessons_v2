04a84f57baca007048ca08d1366799a8d6139131
sanarWH <vkhv0702@gmail.com>
2023-11-29 20:02:47 +0300

    KS-13-2 solution

--- src/main/kotlin/lesson13_task02/lesson13_task02.kt
+++ src/main/kotlin/lesson13_task02/lesson13_task02.kt
@@ -4,17 +4,20 @@ fun main() {
     val user1 = DataBaseV2("Mike", 89507639413)
     user1.getData()
 }
+
 class DataBaseV2(var nameOfUser: String, var number: Long, var nameOfCompany: String? = "") {
 
 
-    fun getData(){
+    fun getData() {
         if (nameOfCompany == null || nameOfCompany == "") nameOfCompany = "не указано"
 
-        println("""
+        println(
+            """
             Имя: $nameOfUser
             Телефон: $number
             Компания: $nameOfCompany
-        """.trimIndent())
+        """.trimIndent()
+        )
     }
 
 }
