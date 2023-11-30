e47b403a58418bd8cb65ed81be650772059ad165
sanarWH <vkhv0702@gmail.com>
2023-11-30 08:09:30 +0300

    KS-10-3 solution

--- src/main/kotlin/lesson10_task03/lesson10_task03.kt
+++ src/main/kotlin/lesson10_task03/lesson10_task03.kt
@@ -3,25 +3,19 @@ package lesson10_task03
 fun main() {
     println("Эта программа поможет вам сгенерировать случайны пароль. \n")
     Thread.sleep(1000)
-    passwordGenerator()
-}
-
-fun passwordGenerator() {
-    val number = 0..9
-    val letter = 'a'..'z'
-    val letterBig = 'A'..'Z'
-    val symbol = '\u0020'..'\u002F'
-    var password: String = ""
     print("Какой длины пароль вы хотите сгененрировать? Ответ: ")
     val length = readln().toInt()
+    println("Ваш сгенерированный пароль: ${passwordGenerator(length)}")
+}
 
-    while (password.length < length) {
+fun passwordGenerator(lengthPassword: Int): String {
+    val number = 0..9
+    val symbol = ' '..'/'
+    var password: String = ""
+
+    while (password.length < lengthPassword) {
         password += number.random()
-        password += letter.random()
-        password += letterBig.random()
-        password += symbol.random()
+        if (password.length % 2 == 0) password += symbol.random()
     }
-
-    val passwordRandom = password.toList().shuffled().joinToString("")
-    print("Ваш случайно сгенерированный пароль из $length символов: $passwordRandom")
+    return password
 }
