7908e30fd115db609939f7900e99fc79eb994d44
sanarWH <vkhv0702@gmail.com>
2023-11-30 10:57:42 +0300

    KS-12-1 solution

--- src/main/kotlin/lesson12_task01/lesson12_task01.kt
+++ src/main/kotlin/lesson12_task01/lesson12_task01.kt
@@ -1,8 +1,8 @@
 package lesson12_task01
 
 fun main() {
-    var day1 = WeatherStat(25, 15, false)
-    var day2 = WeatherStat(21, 14, true)
+    val day1 = WeatherStat(25, 15, false)
+    val day2 = WeatherStat(21, 14, true)
 
     day1.inConsole()
     day2.inConsole()
