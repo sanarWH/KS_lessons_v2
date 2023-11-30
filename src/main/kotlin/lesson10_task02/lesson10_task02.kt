874d7ec0623723d7ea1384805db02e20cd06050e
sanarWH <vkhv0702@gmail.com>
2023-11-30 07:44:49 +0300

    KS-10-2 solution

--- src/main/kotlin/lesson10_task02/lesson10_task02.kt
+++ src/main/kotlin/lesson10_task02/lesson10_task02.kt
@@ -5,12 +5,11 @@ fun main() {
     val userLogin = readln()
     print("Окно регистраици. \nВведите пароль (минимум 4 символа): ")
     val userPassword = readln()
-    inputLength(userLogin)
-    inputLength(userPassword)
+    if (checkPasswordLength(userLogin) && checkPasswordLength(userPassword))
+        println("Добро пожаловать!")
+    else println("Логин или пароль недостаточно длинные")
 }
 
-fun inputLength(name: String) {
-    if (name.length < 4) {
-        return print("Логин или пароль недостаточно длинные")
-    } else return (println("Добро пожаловать"))
+fun checkPasswordLength(incomingString: String): Boolean {
+    return incomingString.length > 3
 }
