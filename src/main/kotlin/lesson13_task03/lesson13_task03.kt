59f0c4278bca9c67d433b5d7941d89072b74aa86
sanarWH <vkhv0702@gmail.com>
2023-11-30 11:50:17 +0300

    KS-13-3 solution

--- src/main/kotlin/lesson13_task03/lesson13_task03.kt
+++ src/main/kotlin/lesson13_task03/lesson13_task03.kt
@@ -1,11 +1,23 @@
 package lesson13_task03
 
 fun main() {
-    var user1 = DataBaseV3("Mike", 89507659104)
-    var user2 = DataBaseV3("Tike", 89507659105)
-    var user3 = DataBaseV3("Pike", 89507659106, "null")
-    var user4 = DataBaseV3("Rike", 89507659106)
-    var user5 = DataBaseV3("Wike", 89507659106)
+    val user1 = DataBaseV3("Mike", 89507659104)
+    val user2 = DataBaseV3("Tike", 89507659105)
+    val user3 = DataBaseV3("Pike", 89507659106, "null")
+    val user4 = DataBaseV3("Rike", 89507659106, "Disk")
+    val user5 = DataBaseV3("Wike", 89507659106)
+    val listOfUser = listOf(user1.getData(), user2.getData(), user3.getData(), user4.getData(), user5.getData())
+    println(listOfUser)
+}
 
+class DataBaseV3(var nameOfUser: String, var number: Long, var nameOfCompany: String? = null) {
 
+    fun getData() {
+        println("""
+            Имя: $nameOfUser
+            Телефон: $number
+            Компания: ${nameOfCompany ?: "не указано"}
+        """.trimIndent()
+        )
+    }
 }
