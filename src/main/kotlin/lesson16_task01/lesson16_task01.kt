5c11fa2e783186a9351741d365d25f3c5eb705f6
sanarWH <vkhv0702@gmail.com>
2023-12-03 23:36:19 +0300

    KS-16-1 solution

---
+++ src/main/kotlin/lesson16_task01/lesson16_task01.kt
@@ -0,0 +1,15 @@
+package lesson16_task01
+
+fun main() {
+    val x = Cube().getRandomNumber()
+    println(x)
+
+}
+
+class Cube() {
+    private val randomNumber = (1..6).random()
+
+    fun getRandomNumber(): Int {
+        return randomNumber
+    }
+}
