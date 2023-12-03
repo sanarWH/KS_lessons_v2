5dd613a5d74f4a79c947a98fa23b138c85f8c4ca
sanarWH <vkhv0702@gmail.com>
2023-12-03 22:38:15 +0300

    KS-15-4 solution

--- src/main/kotlin/lesson15_task04/lesson15_task04.kt
+++ src/main/kotlin/lesson15_task04/lesson15_task04.kt
@@ -4,6 +4,7 @@ fun main() {
     val drum = Instrument("барабан", 3)
     drum.search()
 }
+
 class Instrument(val name: String, val amount: Int) : Search {
     override fun search() {
         println("Поиск комплектующих к инструменту $name")
