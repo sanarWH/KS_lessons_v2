c6d720d948c3e53a57ecc27a02a31a1e89ffca80
sanarWH <vkhv0702@gmail.com>
2023-12-05 19:55:24 +0300

    KS-17-1 solution

--- src/main/kotlin/lesson17_task01/lesson17_task01.kt
+++ src/main/kotlin/lesson17_task01/lesson17_task01.kt
@@ -1,2 +1,36 @@
 package lesson17_task01
 
+fun main() {
+
+    val fish = BabelFish(null)
+
+    println("old value: ${fish.nerveSignalLevel}")
+//    println("old value: ${fish.isTranslated}")
+//    fish.nerveSignalLevel = 400
+//    println("new value: ${fish.nerveSignalLevel}")
+//    println("new value: ${fish.isTranslated}")
+
+    val fish2 = BabelFish(2)
+    println("old value: ${fish2.nerveSignalLevel}")
+
+    val fish3 = BabelFish(21)
+    println("old value: ${fish3.nerveSignalLevel}")
+
+}
+
+class BabelFish(
+    private val coefficient: Int?
+) {
+
+    var isTranslated = false
+
+    var nerveSignalLevel = 200
+        get() = if (coefficient != null) field  * coefficient else field
+        set(value: Int) {
+            field = value
+            if (value > 300){
+                isTranslated = true
+                println("isTranslated = true")
+            }
+        }
+}
