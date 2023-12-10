06ffc94653020a373826e825a91e6a81bb3a7663
sanarWH <vkhv0702@gmail.com>
2023-12-10 23:33:50 +0300

    KS-18-2 solution

---
+++ src/main/kotlin/lesson18_taks02/lesson18_taks02.kt
@@ -0,0 +1,19 @@
+package lesson18_taks02
+
+fun main() {
+    val a = FourSides().roll()
+    val b = SixSides().roll()
+}
+
+open class GetRandomNumber(val sides: Int){
+
+    open fun roll(){
+        val randomNumber = (1..sides).random()
+        println("Выпало число: $randomNumber")
+    }
+}
+class FourSides : GetRandomNumber(4)
+
+class SixSides : GetRandomNumber(6)
+
+class EightSides : GetRandomNumber(8)
