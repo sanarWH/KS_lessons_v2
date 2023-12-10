38e58b96e4fb5a8e75a4309247eb5ad67ca22749
sanarWH <vkhv0702@gmail.com>
2023-12-10 18:31:51 +0300

    KS-17-1

--- src/main/kotlin/lesson17_task01/lesson17_task01.kt
+++ src/main/kotlin/lesson17_task01/lesson17_task01.kt
@@ -1,36 +1,14 @@
-package lesson17_task01
-
 fun main() {
-
-    val fish = BabelFish(null)
-
-    println("old value: ${fish.nerveSignalLevel}")
-//    println("old value: ${fish.isTranslated}")
-//    fish.nerveSignalLevel = 400
-//    println("new value: ${fish.nerveSignalLevel}")
-//    println("new value: ${fish.isTranslated}")
-
-    val fish2 = BabelFish(2)
-    println("old value: ${fish2.nerveSignalLevel}")
-
-    val fish3 = BabelFish(21)
-    println("old value: ${fish3.nerveSignalLevel}")
-
+    val que1 = QuizClass(question = "Какой вопрос?", answer = "Да")
+    println(que1.question)
 }
 
-class BabelFish(
-    private val coefficient: Int?
-) {
-
-    var isTranslated = false
-
-    var nerveSignalLevel = 200
-        get() = if (coefficient != null) field  * coefficient else field
-        set(value: Int) {
-            field = value
-            if (value > 300){
-                isTranslated = true
-                println("isTranslated = true")
-            }
+class QuizClass (question: String?, answer: String?){
+    var question: String? = ""
+        get(){
+            return if (field == null) "Вопрос не задан"
+            else question
         }
+
+    val answer = null
 }
